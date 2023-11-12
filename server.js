15d83f75b9dff7a2271c0c808e78448bcf2ddd1c
gajigaji04 <gajigaji04@naver.com>
2023-11-12 22:42:43 +0900

    axios

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000; // 포트 번호
 
+// "/" 경로에 대한 라우트 정의
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// 정적 파일 제공을 위한 middleware 설정
+app.use(express.static("public"));
+
 // 엔드포인트 정의
 app.get("/api/data", (req, res) => {
   const data = { message: "Hello from the backend!" };
